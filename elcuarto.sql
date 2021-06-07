0deed13c3ff257c363b145ddefca2c4d8ac7ada0
enzo <git@gmail.com>
2021-06-07 18:00:00 -0300

    agregado de todos los archivos

---
+++ elcuarto.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE  IF NOT EXISTS `gestionPedidosCRUD` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `gestionPedidosCRUD`;
+
+
+DROP TABLE IF EXISTS `cliente`;
+
+CREATE TABLE `cliente` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(35) DEFAULT NULL,
+  `apellido` varc
+
+LOCK TABLES `cliente` WRITE;
+
+
+INSERT INTO `cliente` VALUES 
+	(1,'David','Maldonado','david@pildorasinformaticas.es'),
+	(2,'Sandra','López','sandra@pildorasinformaticas.es'),
+	(3,'María','Gómez','maria@pildorasinformaticas.es'),
+	(4,'Antonio','Fernández','antonio@pildorasinformaticas.es'),
+	(5,'Alicia','Martín','alicia@pildorasinformaticas.es');
+
+
+UNLOCK TABLES;
+
+
+
---
+++ eltercero.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `gestionPedidosCRUD` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `gestionPedidosCRUD`;
+
+
+DROP TABLE IF EXISTS `cliente`;
+
+CREATE TABLE `cliente` (
+  `id` int(10) NOT NULL A
